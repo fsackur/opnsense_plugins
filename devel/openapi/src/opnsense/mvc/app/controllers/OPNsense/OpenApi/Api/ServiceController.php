f24aab1b9a796cc4ec12ce6a4704ed271378915b
Freddie Sackur <freddie.git@dustyfox.uk>
2025-04-08 14:40:43 +0100

    add api controller

---
+++ devel/openapi/src/opnsense/mvc/app/controllers/OPNsense/OpenApi/Api/ServiceController.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ *    Copyright (C) 2015 Deciso B.V.
+ *
+ *    All rights reserved.
+ *
+ *    Redistribution and use in source and binary forms, with or without
+ *    modification, are permitted provided that the following conditions are met:
+ *
+ *    1. Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *
+ *    2. Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ *    THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ *    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ *    AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *    AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ *    OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *    POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+namespace OPNsense\OpenApi\Api;
+
+use OPNsense\Base\ApiMutableServiceControllerBase;
+use OPNsense\Core\Backend;
+
+/**
+ * Class ServiceController
+ * @package OPNsense\OpenApi
+ */
+class ServiceController extends ApiMutableServiceControllerBase
+{
+    protected static $internalServiceName = 'openapi';
+    protected static $internalServiceClass = '\OPNsense\OpenApi\OpenApi';
+    protected static $internalServiceTemplate = 'OPNsense/OpenApi';
+    protected static $internalServiceEnabled = 'general.enabled';
+
+    /**
+     * reconfigure with optional stop, generate config and start / reload
+     * @return array response message
+     * @throws \Exception when configd action fails
+     * @throws \ReflectionException when model can't be instantiated
+     */
+    public function reconfigureAction()
+    {
+        $enabled = $this->serviceEnabled();
+        if (!$enabled) {
+            return ['status' => 'disabled'];
+        }
+
+        $backend = new Backend();
+        return $backend->configdRun(escapeshellarg(static::$internalServiceName) . ' reconfigure');
+    }
+
+}
