5f4f69b8201389dd8775977dc295447068a8ca02
Freddie Sackur <freddie.git@dustyfox.uk>
2025-04-11 00:07:06 +0100

    use memoisation to parameterise endpoint tests

--- devel/openapi/tests/test_api.py
+++ devel/openapi/tests/test_api.py
@@ -1,9 +1,17 @@
 #! /usr/bin/env python3
 
+from pytest import mark
+
 from fixtures import *
 
-def test_syslog(api):
-    url = "/syslog/service/stop"
+
+_config: Config = config.__wrapped__()
+_spec = spec.__wrapped__(_config["source_folder"])
+urls = list(_spec._paths.keys())[0:6]
+
+
+@mark.parametrize("url", urls)
+def test_endpoint(url, spec, api):
     response = api.post(url)
     json = response.json()
     assert json == {"response": "STUBBED RESPONSE"}
