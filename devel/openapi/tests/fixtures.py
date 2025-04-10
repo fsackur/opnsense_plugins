720ac66323987c4456396de65456a6905225fa22
Freddie Sackur <freddie.git@dustyfox.uk>
2025-04-10 19:24:59 +0100

    define config for local setup

--- devel/openapi/tests/fixtures.py
+++ devel/openapi/tests/fixtures.py
@@ -1,5 +1,8 @@
 #! /usr/bin/env python3
 
+import os
+import json
+from typing import TypedDict
 from pytest import fixture
 import openapi_spec_validator
 from apispec import APISpec
@@ -7,9 +10,33 @@ from apispec import APISpec
 from loader import generate_openapi_spec
 
 
+class Config(TypedDict):
+    source_folder: str
+
+
 @fixture(scope="session")
-def source_folder():
-    return "/gitroot/upstream/opnsense/core/src/opnsense/"
+def config() -> Config:
+    path = f"{os.path.dirname(__file__)}/config.json"
+    try:
+        with open(path) as file:
+            config_json = file.read()
+
+    except FileNotFoundError as ex:
+        default_config = {a: "" for a in Config.__annotations__.keys()}
+        config_json = json.dumps(default_config, indent=4)
+
+        with open(path, "w") as file:
+            file.write(config_json)
+
+        msg = f"A default config has been created at {path}. Populate the values in the file."
+        raise FileNotFoundError(msg) from None
+
+    return {k: v for k, v in json.loads(config_json).items() if v}  # type: ignore
+
+
+@fixture(scope="session")
+def source_folder(config: Config):
+    return config["source_folder"]
 
 
 @fixture(scope="session")
