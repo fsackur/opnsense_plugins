7b4a3ff03fb51d0b82c4f1e8513c7606e5896c2c
Freddie Sackur <freddie.git@dustyfox.uk>
2025-04-11 16:26:47 +0100

    Revert "another attempt at parameterisation"
    This reverts commit fb4676d17f6d3cc03231a1058426c0e203261249.

--- devel/openapi/tests/conftest.py
+++ devel/openapi/tests/conftest.py
@@ -1,23 +1,6 @@
 
-# from fixtures import spec_from_file
-
-# https://pytest-with-eric.com
-
 def pytest_addoption(parser):
     parser.addoption("--yaml", action="store_true", default=False)
 
 
-# def pytest_generate_tests(metafunc):
-#     # This is called for every test. Only get/set command line arguments
-#     # if the argument is specified in the list of test "fixturenames".
-#     option_value = metafunc.config.option.yaml
-#     if option_value and 'spec' in metafunc.fixturenames:
-#         print(dir(metafunc.config.option))
-#         _spec = spec_from_file.__wrapped__() #"openapi.yml")
-#         metafunc.parametrize("spec", [_spec], ids=["yaml-from-file"])
-
-def pytest_generate_tests(metafunc):
-    print("\nmetafunc:")
-    for k, v in metafunc.__dict__.items():
-        if k.startswith("__"): continue
-        print(f"    {k.ljust(20)}{v}")
+# https://pytest-with-eric.com
--- devel/openapi/tests/test_api.py
+++ devel/openapi/tests/test_api.py
@@ -6,7 +6,6 @@ import pytest
 from pytest import mark, param
 
 from fixtures import *
-from pytest_lazy_fixtures import lf
 
 StrDict = Dict[str, Any]
 
@@ -17,7 +16,7 @@ if "--yaml" in sys.argv:
 else:
     _config: Config = config.__wrapped__()
     _spec = generate_spec.__wrapped__(_config["source_folder"])
-urls = list(_spec._paths.keys())[31:33]
+urls = list(_spec._paths.keys())[0:1]
 
 
 @mark.parametrize("url", urls)
@@ -33,13 +32,6 @@ def test_endpoint(url, spec, api):
         json = response.json()
 
         assert json == expected
-
-
-# @mark.parametrize("path_op", range(9), indirect=True)
-# def test_endpoint(path_op):
-#     path, op = path_op
-#     print(path)
-
 
 
 
