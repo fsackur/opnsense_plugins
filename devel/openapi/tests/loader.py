bc7e1f3f955a45b977cc05d2ead0021bc4a30e3f
Freddie Sackur <freddie.git@dustyfox.uk>
2025-04-15 18:33:18 +0100

    banging my head on validating responses in tests

--- devel/openapi/tests/loader.py
+++ devel/openapi/tests/loader.py
@@ -32,10 +32,32 @@ generate_openapi_spec = sys.modules["generate_openapi_spec"].generate_openapi_sp
 
 
 def load_openapi_spec(path: str) -> APISpec:
+    # print("////////////////////////////////")
     with open(path) as file:
         content = file.read()
     _spec = yaml.load(content, Loader=yaml.CLoader)
+    # print(_spec.keys())
+    # print(_spec["paths"]["/auth/priv/get"])
 
     info = _spec.pop("info")
     version = _spec.pop("openapi")
-    return APISpec(openapi_version=version, **info, **_spec)
+    paths = _spec.pop("paths")
+    components = _spec.pop("components")["schemas"]
+    # print(components)
+
+    spec = APISpec(openapi_version=version, **info)
+
+    for path, op in paths.items():
+        description = op.pop("description")
+        spec.path(path=path, description=description, operations=op)
+
+    for ref, c in components.items():
+        spec.components.schema(ref, c)
+        # print(c)
+        # break
+        # spec.path(path=path, description=description, operations=op)
+
+    # print(dir(spec))
+    # print(list(spec._paths.keys()))
+    # print("////////////////////////////////")
+    return spec
--- devel/openapi/tests/test_api.py
+++ devel/openapi/tests/test_api.py
@@ -1,66 +1,118 @@
 #! /usr/bin/env python3
 
 import sys
-from typing import Dict, Any, Literal
+from typing import Dict, Any, Literal, Hashable, Mapping
 import pytest
 from pytest import mark, param
+from unittest.mock import ANY
+# import openapi_schema_validator
+from openapi_schema_validator import validate
+from referencing import Registry, Resource
 
 from fixtures import *
+from urls import urls
 
 StrDict = Dict[str, Any]
 
 
 
-if "--yaml" in sys.argv:
-    _spec = load_spec_from_file.__wrapped__()
-else:
-    _config: Config = config.__wrapped__()
-    _spec = generate_spec.__wrapped__(_config["source_folder"])
-urls = list(_spec._paths.keys())[40:][0:1]
-print(urls)
+# if "--yaml" in sys.argv:
+#     _spec = load_spec_from_file.__wrapped__()
+# else:
+#     _config: Config = config.__wrapped__()
+#     _spec = generate_spec.__wrapped__(_config["source_folder"])
+# urls = list(_spec._paths.keys())[8:9]
+
+@fixture
+def registry(spec: APISpec) -> Registry:
+    components = spec.components.schemas
+
+    # priv = components["opnsense.auth.priv"]
+    # print(priv)
+    # reg = Registry().with_resource(f"opnsense.auth.priv", Resource.from_contents(priv, default_specification=DRAFT202012))
+
+    # parent_schema = {"components": spec.components}
+    parent_schema = {"components": {"schemas": spec.components.schemas}}
+    # parent_schema = {"schemas": spec.components.schemas}
+
+    schema = Resource(contents=parent_schema, specification=DRAFT202012)
+    reg = Registry().with_resource(uri="/components", resource=schema)
+    return reg
+
+
+from pprint import pprint, pformat
 
 @mark.parametrize("url", urls)
 def test_endpoint(url, spec, api):
     method_op = spec._paths[url]
-    components = spec.components.schemas
-    print("++++++++++++++++++++++++++")
-    print(method_op)
-
     for method, op in [(k, v) for k, v in method_op.items() if k in ("get", "post")]:
         schema = op["responses"]["200"]["content"]["application/json"]["schema"]
-        expected = get_expected_response(schema, components)
-
+        # schema = {"$ref": '#/components/schemas/opnsense.auth.priv'}
+        schema = resolve_schema(schema, spec.components.schemas)
         response = api.call(method, url)
-        json = response.json()
+        # response.raise_for_status()
+        # response_body = {
+        #     "priv": {
+        #         "users": {
+        #             "8f9fbebf-b918-443f-9ea2-4faf264ac1b0": {"value": "freddie", "selected": 0},
+        #             "788305db-e7db-4241-9eb6-51631c6aa616": {"value": "root", "selected": 0},
+        #         },
+        #         "groups": {
+        #             "9172be89-d270-48b6-9482-e7925b4a3ce7": {"value": "admins", "selected": 0}
+        #         },
+        #     }
+        # }
+        response_body = response.json()
+        print(response_body)
+        print(schema)
+        validate(response_body, schema)
 
-        assert json == expected
+
+def resolve_schema(schema: StrDict, components: StrDict) -> StrDict:
+    _schema = {}
+    ref: str | None = schema.get("$ref", None)
+    if ref is not None:
+        model_name = ref.replace("#/components/schemas/", "").split("/", maxsplit=1)[0]
+        _schema = components[model_name]
+    for k, v in schema.items():
+        if k == "$ref":
+            continue
+        if isinstance(v, Mapping):
+            v = resolve_schema(v, components)
+        _schema[k] = v
+    return _schema
 
 
 
-def get_expected_response(schema: StrDict, components: StrDict) -> StrDict:
-    # while "$ref" not in expected:
-    ref: str = schema["$ref"].replace("#/components/schemas/", "")
-    print(ref)
-    if "/" in ref:
-        name, path = ref.split("/", maxsplit=1)
-    else:
-        name = ref
-        path = None
+# def get_expected_response(schema: StrDict, components: StrDict) -> StrDict:
+#     schema = deepcopy(schema)
+#     # while "$ref" not in expected:
+#     ref: str = schema["$ref"].replace("#/components/schemas/", "")
+#     print(ref)
+#     if "/" in ref:
+#         name, path = ref.split("/", maxsplit=1)
+#     else:
+#         name = ref
+#         path = None
 
-    component = components[name]
-    if path:
-        breadcrumbs = path.split("/")
-        for breadcrumb in breadcrumbs:
-            component = component[breadcrumb]
-        # while breadcrumbs:
-        #     prop = breadcrumbs[0]
-        #     if "properties" in component:
-        #         component = component["properties"][prop]
-        #         breadcrumbs = breadcrumbs[1:]
-        #     elif "items" in component:
-        #         component = component["items"]
-        #         # still on the same breadcrumb; go round again
-        #     else:
-        #         raise KeyError(f"could not find {prop} in {component}")
+#     component = components[name]
+#     if path:
+#         breadcrumbs = path.split("/")
+#         for breadcrumb in breadcrumbs:
+#             component = component[breadcrumb]
+#         # while breadcrumbs:
+#         #     prop = breadcrumbs[0]
+#         #     if "properties" in component:
+#         #         component = component["properties"][prop]
+#         #         breadcrumbs = breadcrumbs[1:]
+#         #     elif "items" in component:
+#         #         component = component["items"]
+#         #         # still on the same breadcrumb; go round again
+#         #     else:
+#         #         raise KeyError(f"could not find {prop} in {component}")
 
-    return component["properties"]
+#     return component["properties"]
+
+
+# if __name__ == "__main__":
+#     pytest.
