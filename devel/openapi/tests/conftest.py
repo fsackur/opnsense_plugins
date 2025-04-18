96c132557003db06c60b7d981114f1571b11a13d
Freddie Sackur <freddie.git@dustyfox.uk>
2025-04-18 13:30:09 +0100

    wip

--- devel/openapi/tests/conftest.py
+++ devel/openapi/tests/conftest.py
@@ -3,6 +3,6 @@ def pytest_addoption(parser):
     parser.addoption("--yaml", action="store_true", default=False)
     parser.addoption("--slice")
     parser.addoption("--url")
-
+    parser.addoption("--log-path")
 
 # https://pytest-with-eric.com
